1a4cf27f3a1e232b9bbb14338617d926da3faaba
IljaKulikov24 <KulikovvvIlja@gmail.com>
2024-11-16 21:34:43 +0400

    Переопределение методов в IdentityFunction и покрытие класса тестами

--- LR3/src/main/java/ru/ssau/tk/_division_/_lr3_/functions/IdentityFunction.java
+++ LR3/src/main/java/ru/ssau/tk/_division_/_lr3_/functions/IdentityFunction.java
@@ -1,9 +1,32 @@
 package ru.ssau.tk._division_._lr3_.functions;
 
-public class IdentityFunction implements MathFunction {
+import java.util.Random;
+
+public class  IdentityFunction implements MathFunction, Cloneable {
 
     @Override
     public double apply(double x) {
         return x;
+    }
+
+    @Override
+    public String toString() {
+        return "(IdentityFunction) Объекты этого класса выполняют тождественное преобразование";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return o instanceof IdentityFunction;
+    }
+    
+    @Override
+    public int hashCode() {
+        Random RANDOM = new Random();
+        return RANDOM.nextInt();
+    }
+
+    @Override
+    public IdentityFunction clone() {
+        return new IdentityFunction();
     }
 }
