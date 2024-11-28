594ee68bfb8e8f7e4e628d2489ec30c548fbc51d
IljaKulikov24 <KulikovvvIlja@gmail.com>
2024-11-29 03:13:37 +0400

    Добавление завершенного класса FunctionsIO

--- LR5/src/main/java/ru/ssau/tk/_division_/_lr5_/functions/factory/TabulatedFunctionFactory.java
+++ LR5/src/main/java/ru/ssau/tk/_division_/_lr5_/functions/factory/TabulatedFunctionFactory.java
@@ -4,5 +4,5 @@ import ru.ssau.tk._division_._lr5_.functions.TabulatedFunction;
 
 public interface TabulatedFunctionFactory {
 
-    public TabulatedFunction create(double[] xValues, double[] yValues);
+    TabulatedFunction create(double[] xValues, double[] yValues);
 }
---
+++ LR5/src/main/java/ru/ssau/tk/_division_/_lr5_/io/FunctionsIO.java
@@ -0,0 +1,8 @@
+package ru.ssau.tk._division_._lr5_.io;
+
+final class FunctionsIO {
+
+    private FunctionsIO(){
+        throw new UnsupportedOperationException();
+    }
+}
